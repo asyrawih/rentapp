817304445d911f9244e0c6f171c87543c90cd750
hasyrawi@gmail.com <hasyrawi@gmail.com>
2019-11-01 22:15:54 +0800

    Add Cors Headers

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -12,9 +12,12 @@ class OwnerController extends Controller
      * Headers
      */
 
-    public $headers = [
-        'Content-Type'  => 'application/json',
-        'X-Request-with' => 'XMLHttpRequest'
+   public $headers = [
+        'Access-Control-Allow-Origin'      => '*',
+        'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
+        'Access-Control-Allow-Credentials' => 'true',
+        'Access-Control-Max-Age'           => '86400',
+        'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
     ];
 
     /**
